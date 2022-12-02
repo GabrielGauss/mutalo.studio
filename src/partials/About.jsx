39f15e139f244777bc03094cf49d6a47ab9b310b
GabrielGauss <gabriel.veron134@gmail.com>
2022-12-02 20:56:21 +0100

    segundo commit

---
+++ src/partials/About.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div
+      className=" flex flex-col md:flex-row  py-30 mb-20 mt-40 bg-gradient-to-t from-gray-200 to-white"
+      data-aos="zoom-x-out"
+      data-aos-delay="150"
+      data-aos-
+    >
+      <div className="absolute left-0 right-0 m-auto w-px p-px h-20 bg-gray-200 transform -translate-y-1/2"></div>
+
+      <h1 className="text-4xl text-gray-800 text-b my-10 text-center font-extrabold  tracking-tighter md:text-5xl basis-1/3">
+        A little about us
+      </h1>
+      {/* <span className="hide md:text-10xl text-gray-300">|</span> */}
+      <p className=" mb-20 p-10  text-xl text-gray-500 text-left basis-2/3 md:py-20  md:px-40">
+        Mutalo is a succesfull young marketing agency that loves their clients.
+        <strong>Our mission and vision</strong> is to provide the best ROI
+        possilbe while building the relationship between client and agency along
+        the way. We are a curious agency, we love to find new ways of doing
+        things, testing our strategies and getting inspired by what we do..{" "}
+        <strong>Our goal</strong> is to build strong relationship with our
+        clients , holding them at highest value in everything we do.
+      </p>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/partials/Footer.jsx
@@ -0,0 +1,210 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import SocialLinks from "./SocialLinks";
+
+function Footer() {
+  return (
+    <footer>
+      <div className="max-w-6xl mx-auto px-4 sm:px-6 ">
+        {/* Top area: Blocks */}
+        <div className="grid sm:grid-cols-12 gap-8 py-8 md:py-12 border-t border-gray-200">
+          {/* 1st block */}
+          <div className="sm:col-span-12 lg:col-span-3">
+            <div className="mb-2">
+              {/* Logo */}
+              <Link
+                to="/"
+                className="inline-block text-left"
+                aria-label="Mutalo"
+              >
+                <img
+                  src="./src/images/mutalo-header-logo.png"
+                  alt="mutalo-logo"
+                />
+                {/* <svg className="w-8 h-8" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
+                  <defs>
+                    <radialGradient cx="21.152%" cy="86.063%" fx="21.152%" fy="86.063%" r="79.941%" id="footer-logo">
+                      <stop stopColor="#4FD1C5" offset="0%" />
+                      <stop stopColor="#81E6D9" offset="25.871%" />
+                      <stop stopColor="#338CF5" offset="100%" />
+                    </radialGradient>
+                  </defs>
+                  <rect width="32" height="32" rx="16" fill="url(#footer-logo)" fillRule="nonzero" />
+                </svg> */}
+              </Link>
+            </div>
+            <div className="text-sm text-gray-600">
+              <Link
+                to="#"
+                className="text-gray-600 hover:text-gray-900 hover:underline transition duration-150 ease-in-out"
+              >
+                Terms
+              </Link>{" "}
+              ·{" "}
+              <Link
+                to="#"
+                className="text-gray-600 hover:text-gray-900 hover:underline transition duration-150 ease-in-out"
+              >
+                Privacy Policy
+              </Link>
+            </div>
+          </div>
+
+          {/* 2nd block */}
+          <div className="sm:col-span-6 md:col-span-3 lg:col-span-2">
+            <h6 className="text-gray-800 font-medium mb-2">Solutions</h6>
+            <ul className="text-sm">
+              <li className="mb-2">
+                <Link
+                  to="#"
+                  className="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out"
+                >
+                  Web Studio
+                </Link>
+              </li>
+              <li className="mb-2">
+                <Link
+                  to="#"
+                  className="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out"
+                >
+                  Branding & Design
+                </Link>
+              </li>
+              <li className="mb-2">
+                <Link
+                  to="#"
+                  className="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out"
+                >
+                  E-Marketing
+                </Link>
+              </li>
+              <li className="mb-2">
+                <Link
+                  to="#"
+                  className="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out"
+                >
+                  Analitics & Insights
+                </Link>
+              </li>
+            </ul>
+          </div>
+
+          {/* 3rd block */}
+          <div className="sm:col-span-6 md:col-span-3 lg:col-span-2">
+            <h6 className="text-gray-800 font-medium mb-2">Resources</h6>
+            <ul className="text-sm">
+              <li className="mb-2">
+                <Link
+                  to="#"
+                  className="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out"
+                >
+                  Tutorials & Guides
+                </Link>
+              </li>
+              <li className="mb-2">
+                <Link
+                  to="#"
+                  className="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out"
+                >
+                  Blog
+                </Link>
+              </li>
+              <li className="mb-2">
+                <Link
+                  to="#"
+                  className="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out"
+                >
+                  Support Center
+                </Link>
+              </li>
+              <li className="mb-2">
+                <Link
+                  to="#"
+                  className="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out"
+                >
+                  Partners
+                </Link>
+              </li>
+            </ul>
+          </div>
+
+          {/* 4th block */}
+          <div className="sm:col-span-6 md:col-span-3 lg:col-span-2">
+            <h6 className="text-gray-800 font-medium mb-2">Company</h6>
+            <ul className="text-sm">
+              <li className="mb-2">
+                <Link
+                  to="#"
+                  className="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out"
+                >
+                  Home
+                </Link>
+              </li>
+              <li className="mb-2">
+                <Link
+                  to="#"
+                  className="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out"
+                >
+                  About us
+                </Link>
+              </li>
+
+              <li className="mb-2">
+                <Link
+                  to="#"
+                  className="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out"
+                >
+                  Pricing
+                </Link>
+              </li>
+              <li className="mb-2">
+                <Link
+                  to="#"
+                  className="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out"
+                >
+                  Privacy Policy
+                </Link>
+              </li>
+            </ul>
+          </div>
+
+          {/* 5th block */}
+          <div className="sm:col-span-6 md:col-span-3 lg:col-span-3 flex flex-col gap-3">
+            <h6 className="text-gray-800 font-medium mb-2">Contact Info</h6>
+            <a
+              href=""
+              className=" text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out"
+            >
+              Hello@mutalo.com
+            </a>
+            <a
+              href=""
+              className="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out"
+            >
+              +39 351 533 2816
+            </a>
+          </div>
+        </div>
+
+        {/* Bottom area */}
+        <div className="md:flex md:items-center md:justify-between py-4 md:py-8 border-t border-gray-200">
+          <SocialLinks />
+
+          {/* Copyrights note */}
+          <div className="text-sm text-violet-600 mr-4">
+            Made by{" "}
+            <a
+              className="text-Pink-600 hover:underline"
+              href="https://mutalo.studio/"
+            >
+              MUTALO.studio
+            </a>
+            . All rights reserved.
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
