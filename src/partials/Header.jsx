e3f10f3b61d27f129e4d804301c019103d187baf
GabrielGauss <gabriel.veron134@gmail.com>
2022-12-26 23:11:35 +0100

    arreglado los logos de las redes

--- src/partials/Header.jsx
+++ src/partials/Header.jsx
@@ -97,14 +97,16 @@ export default function Header() {
               ></img>
             </a>
           </div>
-          <div className="-my-2 -mr-2 md:hidden">
-            <Modal />
-          </div>
-          <div className="-my-2 -mr-2 md:hidden">
-            <Popover.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
-              <span className="sr-only">Open menu</span>
-              <Bars3Icon className="h-6 w-6" aria-hidden="true" />
-            </Popover.Button>
+          <div className="flex items-center justify-end gap-6">
+            <div className="-my-2 -mr-2 md:hidden">
+              <Modal />
+            </div>
+            <div className="-my-2 -mr-2 md:hidden">
+              <Popover.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                <span className="sr-only">Open menu</span>
+                <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+              </Popover.Button>
+            </div>
           </div>
           <Popover.Group as="nav" className="hidden space-x-10 md:flex">
             <Popover className="relative">
